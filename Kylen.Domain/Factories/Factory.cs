4b931e3b5116333514f713cf5df76adcf86699ce
Henrik Forsberg <Henrik.Forsberg@collectorbank.se>
2018-12-12 12:58:09 +0100

    Added factory to domain.

---
+++ Kylen.Domain/Factories/Factory.cs
@@ -0,0 +1,29 @@
+﻿using Kylen.Domain.Repository;
+using Kylen.Domain.Services;
+
+namespace Kylen.Domain.Factories
+{
+    public class Factory
+    {
+        Service _service;
+        public Factory(IRepository repository)
+        {
+            _service = new Service(repository);
+        }
+        public object GetResult(string command)
+        {
+            switch (command)
+            {
+                case "/kylstatus":
+                    return GetDrinkStatus();
+                default:
+                    return null;
+            }
+        }
+
+        private object GetDrinkStatus()
+        {
+            return _service.GetDrinkStatuses();
+        }
+    }
+}
